2d2e4086cf30a61bf113112f7b450cbcf5a5716f
Anuj Kalia <akalia@cs.cmu.edu>
2018-02-05 17:21:47 -0700

    Fix run_batch script

--- bench/run_batch.sh
+++ bench/run_batch.sh
@@ -1,3 +1,1 @@
-sudo perf stat numactl --physcpubind=0 --membind=0 ./bench_batch .*Strasse.* --set gdk_nr_threads=10
-
-#sudo numactl --physcpubind=0 --membind=0 ./bench_batch .*Strasse.* --set gdk_nr_threads=10
+sudo numactl --physcpubind=0 --membind=0 ./bench_batch .*Strasse.*
